42a4a7d03950e791c30c8af1c57637982556ee61
Felps-26 <filipiramos4152@gmail.com>
2025-08-12 12:22:43 -0300

    .

--- Lessons/struct.c
+++ Lessons/struct.c
@@ -7,11 +7,12 @@ typedef struct {
     int denominator;
 } Fraction;
 
-
 Fraction createFraction(int num, int den);
 void exibitFraction(Fraction f);
 int MCDcalculator(int A, int B);
 Fraction simplifyFraction(Fraction f);
+Fraction sumFractions(Fraction f1, Fraction f2);
+Fraction subFraction(Fraction f1, Fraction f2);
 
 int main() {
     system("cls");
@@ -20,13 +21,27 @@ int main() {
     F1 = simplifyFraction(F1);
     exibitFraction(F1);
     printf("\n");
+
     Fraction F2 = createFraction(-24, 30);
     F2 = simplifyFraction(F2);
     exibitFraction(F2);
     printf("\n");
+
     Fraction F3 = createFraction(37, 42);
     F3 = simplifyFraction(F3);
     exibitFraction(F3);
+    printf("\n");
+
+    Fraction F4 = createFraction(5, 6);
+    Fraction F5 = createFraction(4, 9);
+    Fraction F6 = sumFractions(F4, F5);
+    exibitFraction(F6);
+    printf("\n");
+
+    Fraction F7 = subFraction(F4, F5);
+    exibitFraction(F7);
+    printf("\n");
+
 
     return 0;
 }
@@ -60,4 +75,20 @@ Fraction simplifyFraction(Fraction f) {
     f.numerator = f.numerator / mcd;
     f.denominator = f.denominator / mcd;
     return f;
+}
+
+Fraction sumFractions(Fraction f1, Fraction f2) {
+    Fraction Answer;
+    Answer.denominator = f1.denominator * f2.denominator;
+    Answer.numerator = f1.numerator * f2.denominator + f2.numerator * f1.denominator;
+    Answer = simplifyFraction(Answer);
+    return Answer;
+}
+
+Fraction subFraction(Fraction f1, Fraction f2) {
+    Fraction Answer;
+    Answer.denominator = f1.denominator * f2.denominator;
+    Answer.numerator = f1.numerator * f2.denominator - f2.numerator * f1.denominator;
+    Answer = simplifyFraction(Answer);
+    return Answer;
 }
