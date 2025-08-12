727908949c844bbafe49ae34b49f9f44e4d9dee6
Felps-26 <filipiramos4152@gmail.com>
2025-08-12 11:37:42 -0300

    fraction_structexample

--- Lessons/struct.c
+++ Lessons/struct.c
@@ -2,47 +2,62 @@
 #include <stdlib.h>
 #include <string.h>
 
-//typedef struct Person {
-//    char Name[150];
-//    char Tel[14];
-//    char adress[200];
-//} Person;
-//
-//typedef int Integer;
-//
-//int main() {
-//    
-//    Person P;
-//    Integer I;
-//    
-//    strcpy(P.Name, "Maria Alves");
-//    strcpy(P.Tel, "(61) 123456789");
-//    strcpy(P.adress, "Rua das Flores, 123");
-//
-//    printf("\nName: %s\n", P.Name);
-//    printf("Telephone: %s\n", P.Tel);
-//    printf("Address: %s\n", P.adress);
-//
-//    return 0;
-//}
-
 typedef struct {
-    float numerator;
-    float denominator;
+    int numerator;
+    int denominator;
 } Fraction;
 
-Fraction createFraction(float num, float den) {
+
+Fraction createFraction(int num, int den);
+void exibitFraction(Fraction f);
+int MCDcalculator(int A, int B);
+Fraction simplifyFraction(Fraction f);
+
+int main() {
+    system("cls");
+
+    Fraction F1 = createFraction(24, 30);
+    F1 = simplifyFraction(F1);
+    exibitFraction(F1);
+    printf("\n");
+    Fraction F2 = createFraction(-24, 30);
+    F2 = simplifyFraction(F2);
+    exibitFraction(F2);
+    printf("\n");
+    Fraction F3 = createFraction(37, 42);
+    F3 = simplifyFraction(F3);
+    exibitFraction(F3);
+
+    return 0;
+}
+
+Fraction createFraction(int num, int den) {
     Fraction f;
     f.numerator = num;
     f.denominator = den;
     return f;
 }
 
-int main(){
+void exibitFraction(Fraction f) {
+    printf("Fraction: %d / %d\n", f.numerator, f.denominator);
+    printf("Result: %.2f\n", (float)f.numerator / f.denominator);
+}
 
-   Fraction f1 = createFraction(7.0, 2.0);
-   printf("Fraction: %.1f / %.1f\n", f1.numerator, f1.denominator);
-   printf("result: %.1f\n", f1.numerator / f1.denominator);
+int MCDcalculator(int A, int B){
+    if(A < 0) A = -A;
+    if(B < 0) B = -B;
 
-    return 0;
+    while(B != 0) {
+       int temp = B;
+       B = A % B;
+       A = temp;
+    }
+    return A;
+}
+
+Fraction simplifyFraction(Fraction f) {
+    int mcd = MCDcalculator(f.numerator, f.denominator);
+    f.numerator = f.numerator / mcd;
+    f.denominator = f.denominator / mcd;
+    return f;
 }
